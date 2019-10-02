16c97539bd78ef67eaf9328b42fda61035674509
jose <neto.silvacem@gmail.com>
2019-10-02 10:53:57 -0300

    verificando

--- JavaFrame/src/br/com/senac/pi/ui/Main.java
+++ JavaFrame/src/br/com/senac/pi/ui/Main.java
@@ -1,6 +1,7 @@
 package br.com.senac.pi.ui;
 //só um teste
 //outro teste
+//testando permi
 public class Main {
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(() -> {
