d07316dbe4ddb5beafd2593ac934a1fd41c01c6e
joao.lvsilva <joao.lvsilva@CAS527196W1049.senacsp.edu.br>
2019-10-10 20:14:44 -0300

    modificando entidade usuario, para receber rg, cpf e adicionado construtores.

--- javaFrame-entidades/src/br/com/senac/pi/entidades/Usuario.java
+++ javaFrame-entidades/src/br/com/senac/pi/entidades/Usuario.java
@@ -15,6 +15,8 @@ public class Usuario {
     private String email;
     private String senha;
     private String setor;
+    private String rg;
+    private String cpf;
 
     public Usuario(int id, String nome, String email, String senha, String setor) {
         this.id = id;
@@ -24,9 +26,38 @@ public class Usuario {
         this.setor = setor;
     }
     
+     public Usuario(String nome,String rg,String cpf ,String email, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.setor = setor;
+    }
+    public Usuario(){}
+
+    
+
+   
+  
     
     public int getId() {
         return id;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
     }
         
     public String getNome() {
